1fdccd9410d8c40fb4bd8035956cd491502a7f8f
Sebastian <sebastiancor7218@gmail.com>
2019-12-23 13:28:08 -0300

    Added filename to url

--- app.py
+++ app.py
@@ -12,11 +12,20 @@ def upload():
     template = """<!DOCTYPE html>
                 <html>
                 <body>
-                <form action="/api/upload" method="post" enctype="multipart/form-data">
+                <form action="#" method="post" id="frm" enctype="multipart/form-data">
                         Select File to upload:
                         <input type="file" name="fileToUpload" id="fileToUpload">
-                        <input type="submit" value="Upload Image" name="submit">
+                        <input type="submit" value="Upload Image" id="sbmit" name="submit" disabled>
                 </form>
                 </body>
+                <script>
+				document.getElementById('fileToUpload').addEventListener("change", changeAction);
+				const frm = document.getElementById('frm');
+				const sbmit = document.getElementById('sbmit');
+				function changeAction(e){
+					frm.action = "/api/upload/" + e.target.files[0].name;
+					sbmit.disabled = !(e.target.files[0].name);
+				}
+				</script>
                 </html>"""
     return Response(template, status_code=200, headers={"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"})
