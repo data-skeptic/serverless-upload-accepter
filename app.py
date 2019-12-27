e20794530ec972376268486d34d285e8643215c8
Sebastian <sebastiancor7218@gmail.com>
2019-12-27 19:36:52 -0300

    Added verification logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from chalice import Chalice, Response
 from datetime import datetime
+import base64
 import filetype
 import logging
 import cgi
@@ -12,6 +13,7 @@ import os
 app = Chalice(app_name='Test')
 app.debug = True
 app.log.setLevel(logging.DEBUG)
+app.api.binary_types.append("multipart/form-data")
 
 s3_client = boto3.client('s3')
 BUCKET = 'sebastian-testbucket'
@@ -46,7 +48,7 @@ def upload():
 def parse_form_data():
     request_body = app.current_request._body
     if isinstance(request_body, str):
-        request_body = request_body.encode()
+        request_body = base64.b64decode(request_body)
     content_type_obj = app.current_request.to_dict()['headers']['content-type']
     content_type, property_dict = cgi.parse_header(content_type_obj)
     property_dict['boundary'] = property_dict['boundary'].encode('utf-8')
@@ -66,6 +68,7 @@ get_file_name = lambda full_name: os.path.splitext(full_name)[0]
 @app.route('/upload/{file_name}', methods=['POST'], cors=True, content_types=['multipart/form-data'])
 def upload_file(file_name):
     # Form data pare to file
+    print(app.current_request._body)
     parsed_file_data = ''
     try:
         parsed_file_data = parse_form_data()
@@ -95,7 +98,17 @@ def upload_file(file_name):
         s3 = boto3.resource('s3')
         file_path = datetime.today().strftime('uploads/%Y/%m/%d/{}/{}.pdf').format(unix_timestamp_ms(), get_file_name(file_name))
         s3.Bucket(BUCKET).put_object(Key=file_path, Body=parsed_file_data)
-        return Response(body={'Success': 'Uploaded file to s3'}, status_code=200)
+        # return Response(body={'Success': 'Uploaded file to s3'}, status_code=200)
 
     except:
         return Response(body={'error': 'Save file to S3 error!'}, status_code=500)
+
+    # # Check file type of s3
+    # try:
+    #     kind = filetype.guess(tmp_file_path)
+    #     if kind.mime == 'application/pdf':
+    #         return Response(body={'Success': 'This is a pdf'}, status_code=200)
+    #     else:
+    #         return Response(body={'Success': 'This is not a pdf'}, status_code=200)
+    # except:
+    #     return Response(body={'error': 'This is not a pdf'}, status_code=200)
